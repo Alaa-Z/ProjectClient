465f5ce3c4054c26e3607f5297730d2c58fc1247
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-19 16:50:28 +0200

    make login with axios

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,18 +1,53 @@
-function LoginForm() {
-    return(
-      <form>
-        <label>
-          Email:
-          <input type="email" />
-        </label>
-        <label>
-          Password:
-          <input type="password"/>
-        </label>
-        <button type="submit"> Register </button>
-      </form>
-)}
-export default LoginForm;
+import { useState } from 'react';
+import axios from 'axios';
 
- 
- 
+import { loginEndPoint } from '../config/endpoints';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // error messages 
+  const [error, setError] = useState('');
+
+  // to handel the form submit 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(loginEndPoint, {
+          email,
+          password,
+      });
+      console.log(response)
+      console.log(response.data)
+      //if registration is successful
+      setError('');
+    } catch (error) {
+        if(error){
+            console.log(error);
+            setError(error.response.data);
+            // show error message only for 3 seconds
+            setTimeout(() => {
+            setError('');
+            }, 2000); 
+        } 
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+          Email:
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+      {/* if the messages exist show them */}
+      {error && <p>{error}</p>}
+    </form>
+  );
+}
+
+  
+export default LoginForm;
