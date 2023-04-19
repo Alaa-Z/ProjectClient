71a6f62c121ac8fa4795cf15e3f2f115e9a301b6
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-19 16:43:40 +0200

    make post to register user using axios

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,21 +1,64 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+import { registerEndPoint } from '../config/endpoints';
+
+
 function RegisterForm() {
-    return(
-        <form>
-            <label>
+    // create a user 
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   // error and success messages 
+   const [error, setError] = useState('');
+   const [success, setSuccess] = useState(false);
+
+   // to handel the form submit 
+   const handleSubmit = async (e) => {
+     e.preventDefault();
+     try {
+       const response = await axios.post(registerEndPoint, {
+            name,
+            email,
+            password,
+       });
+       console.log(response)
+       //if registration is successful
+       setSuccess(true);
+       setError('');
+     } catch (error) {
+        if(error){
+            console.log(error);
+            setSuccess(false);
+            setError(error.response.data);
+            // show error message only for 3 seconds
+            setTimeout(() => {
+            setError('');
+            }, 2000); 
+        } 
+     }
+   };
+   return (
+    <form onSubmit={handleSubmit}>
+        <label>
             Name:
-            <input type="text" /> <br></br>
-            </label>
-            <label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
             Email:
-            <input type="email" /> <br></br>
-            </label>
-            <label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
             Password:
-            <input type="password"/><br></br>
-            </label>
-            <button type="submit"> Register </button>
-        </form>
-    )
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Register</button>
+        {/* if the messages exist show them */}
+        {error && <p>{error}</p>}
+        {success && <p>User added successfully!</p>}
+     </form>
+   );
 }
- 
-  export default RegisterForm;
+
+  
+export default RegisterForm;
---
+++ config/endpoints.js
@@ -0,0 +1,6 @@
+// REGISTER API
+const registerEndPoint = 'http://localhost:5000/api/user/register';
+// Log in API
+const loginEndPoint = 'http://localhost:5000/api/user/login';
+
+export {registerEndPoint, loginEndPoint}
