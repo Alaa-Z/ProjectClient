8c1788c9113b9aaa7ce360a0e46e6fa9b0f8fb2d
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-05-01 10:11:33 +0200

    add new component and basics style to homepage

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,61 @@
 import Link from 'next/link'
+import { useState } from 'react';
+// Style 
+import styles from '../styles/Header.module.scss'
+// React icons 
+import {GiHamburgerMenu} from 'react-icons/gi';
+import HomeHeader from './HomeHeader';
+
 
 function Header() {
+  // for  navbar in small screens 
+  const [showNav, setShowNav] = useState(false)
+
+  // toggle the value of showNav
+  const handleShowNavbar = () => {
+    setShowNav(!showNav)
+  }
+
   return (
-    <header>
+    <>
+    <header className={styles.header}>
       <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              About
-            </Link>
-          </li>
-        </ul>
+        <div className={styles.navContainer} >
+            <div className={styles.logo}>
+              <h1>logo</h1>
+            </div>
+            <div className={styles.menuIcon} onClick={handleShowNavbar}>
+            <GiHamburgerMenu />
+          </div>
+          {/* shownor hide based on showNav */}
+          <div className={`${styles.listitems} ${showNav ? styles.show : ""}`}>
+            <ul>
+              <li>
+                <Link href="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  About
+                </Link>
+              </li> <li>
+                <Link href="/">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
+    <HomeHeader /> 
+    </>
   )
 }
 
---
+++ components/HomeHeader.js
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import booksImg from '../public/books.jpg'
+
+// Style 
+import styles from '../styles/HomeHeader.module.scss'
+
+export default function HomeHeader() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.text}>
+                <h2> Neque porro quisquam est qui dolorem ipsum porro quisquam est qui dolorem ipsum </h2>
+                <p>  Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.</p>
+                <form>
+                <input type="text" placeholder="Search for a book" />
+                <button>  Search  </button>
+                </form>
+            </div>
+            <div className={styles.imageWrapper}>
+                <Image className={styles.image}
+                    src={booksImg}
+                    alt="Picture of books"
+                    width="350px"
+                    height="300px"
+                    layout="responsive" 
+                />
+            </div>
+        </div> 
+    )
+} 
