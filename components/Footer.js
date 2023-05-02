02903f164a0a4f67a49f923dbb3691bd5f41c496
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-05-02 11:10:27 +0200

    add style to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,31 @@
+import styles from '../styles/Footer.module.scss'
+import { MdEmail } from 'react-icons/md';
+
+
 export default function Footer() {
     return (
-      <footer>
-        this is a footer
+      <footer className={styles.footer}>
+        <div className={styles.content}>
+            <h3>Lorem ipsum dolor </h3>
+            <p>Lorem ipsum dolor Lorem ipsum dolor s</p>
+            <ul className={styles.socials}>
+              <li><a href="#"> <MdEmail className={styles.icon}/></a></li>
+              <li><a href="#"> <MdEmail className={styles.icon}/></a></li>
+              <li><a href="#"> <MdEmail className={styles.icon}/></a></li>
+              <li><a href="#"> <MdEmail className={styles.icon}/></a></li>
+            </ul>
+        </div>
+        <div className={styles.footerBottom}>
+            <p>copyright &copy; <a href="#">Alaa Zaza</a>  </p>
+              <div className={styles.menu}>
+                <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="">About</a></li>
+                  <li><a href="">About</a></li>
+                  <li><a href="">About</a></li>
+                </ul>
+              </div>
+        </div>
       </footer>
     )
 } 
