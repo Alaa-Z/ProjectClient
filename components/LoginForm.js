c9d8db179343deae45aa0b69deecb4c84fe2177f
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-05-02 16:37:05 +0200

    modify loginform

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -3,8 +3,12 @@ import axios from 'axios';
 import jwt_decode from "jwt-decode";
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
+import Link from 'next/link';
 // Style 
 import styles from '../styles/LoginFrom.module.scss'
+// Icons
+import { MdEmail } from 'react-icons/md';
+import { RiLockPasswordFill} from 'react-icons/ri';
 
 import { loginEndPoint } from '../config/endpoints';
 
@@ -57,28 +61,33 @@ function LoginForm() {
       <form className={styles.form} onSubmit={handleSubmit} >
         <div className={styles.inputField}> 
           <label className={styles.label}>
-            E-mail:
+            Email:
           </label>
           <br></br>
-          <input type="email" placeholder="Enter Your E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
+          {email ? null : <MdEmail className={styles.icon}  />}
+          <input type="email" placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className={styles.inputField}> 
           <label className={styles.label}>
             Password:
           </label>
           <br></br>
+          {password ? null : <RiLockPasswordFill className={styles.icon}  />}
           <input type="password"
             value={password} 
-            placeholder="Enter Your Password"
+            placeholder="Enter your password"
             onChange={(e) => setPassword(e.target.value)} />
           </div>
          <button type="submit">Login</button>
        {/* if the messages exist show them */}
       {error && <p>{error}</p>}
     </form>
+    <div className={styles.signup}>
+      Create an account <Link href="/register"> Signup now</Link>
+    </div>
   </div>
    
   );
