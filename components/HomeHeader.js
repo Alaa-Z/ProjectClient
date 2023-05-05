4515943a9df14ed9e4c516489bd5205a86b953ee
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-05-05 18:59:18 +0200

    fix code

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import booksImg from '../public/books.jpg'
 import { useRouter } from 'next/router';
-
 // Style 
 import styles from '../styles/HomeHeader.module.scss'
 
@@ -10,25 +9,28 @@ export default function HomeHeader() {
 
     return (
         router.pathname === '/' ?
-        <div className={styles.container}>
-            <div className={styles.text}>
-                <h2> Neque porro quisquam est qui dolorem ipsum porro quisquam est qui dolorem ipsum </h2>
-                <p>  Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.</p>
-                <form>
-                <input type="text" placeholder="Search for a book" />
-                <button>  Search  </button>
-                </form>
-            </div>
-            <div className={styles.imageWrapper}>
-                <Image className={styles.image}
-                    src={booksImg}
-                    alt="Picture of books"
-                    width="350px"
-                    height="300px"
-                    layout="responsive" 
-                />
-            </div>
-        </div> 
+        <div className={styles.containerHeader} >
+            <div className={styles.container}>
+                <div className={styles.text}>
+                    <h2> Neque porro quisquam est qui dolorem ipsum porro quisquam est qui dolorem ipsum </h2>
+                    <p>  Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.</p>
+                    <form>
+                    <input type="text" placeholder="Search for a book" />
+                    <button>  Search  </button>
+                    </form>
+                </div>
+                <div className={styles.imageWrapper}>
+                    <Image className={styles.image}
+                        src={booksImg}
+                        alt="Picture of books"
+                        width="350px"
+                        height="300px"
+                        layout="responsive" 
+                    />
+                </div>
+            </div> 
+        </div>
+      
         : null
     )
 }  
